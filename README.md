2d550cc54d0d7e44e9e11b4f00780fb4eb03cd1b
CDisidoro <cdisidoro99@gmail.com>
2023-10-28 18:07:28 +0200

    Creacion del proyecto Spring Boot + Dockerizacion

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Quicktask-server
+# GetTasksDone-server
 Servidor y BD del TFG de GTD para el curso 2023-24 de la UCM. La BD se utiliza una imagen estándar de mysql importada en el docker-compose
---
+++ src/main/java/com/gettasksdone/controller/RootController.java
@@ -0,0 +1,25 @@
+package main.java.com.gettasksdone.controller;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.google.common.hash.Hashing;
+
+@RestController
+@RequestMapping("/api")
+public class RootController {
+    @GetMapping("/test")
+    public String prueba(){
+        return "El API funciona";
+    }    
+}
