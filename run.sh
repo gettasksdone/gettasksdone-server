d037ead007adf7a8c333f15c131babe908f02a7e
sudosu <jcandela@ucm.es>
2024-02-22 13:55:02 +0100

    sesion 1 22-02-2024

--- run.sh
+++ run.sh
@@ -1,22 +1,94 @@
 #!/bin/bash
 
-echo "¿Deseas desplegar el proyecto para el frontend (S/n)?"
+# Problemática de la partimos:
+# debe poder desplegarse el proyecto una vez en el servidor
+# debe permitirse actualizar un los contendores
+# debe tener la version de desarollo para el backend
+
+#anotaciones:
+# es mejor crear funciones para no tener redundancias.
+# añadida las anotaciones de David
+# comprobar que la base de datos no se modifica y que se mantiene el servidor
+#  -> comprobamos que da aqui un fallo (revisarlo)
+function confirmacion() {
+
+    read -r -p "¿Estás seguro de tu elección [S/n]? " confirmacion
+    confirmacion=${confirmacion,,}
+    if [[ $confirmacion =~ ^(s|si|S|SI|)$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function mostrar_menu() {
+    echo "Selecciona una opción:"
+    echo "1) Actualizar contenedor del frontend"
+    echo "2) construir contenedores en el servidor desde cero"
+    echo "3) Modo Desarrollador Backend"
+    echo "4) Borrar todo (PELIGRO)"
+    echo "0) Para salir"
+}
 
 DOCKER_COMPOSE_FILE_FRONTED=".docker-compose/docker-compose.fronted.yml"
 DOCKER_COMPOSE_DEVELOP="docker-compose.dev.yml"
 
-# Lee la entrada del usuario con 'S' como opción por defecto
-read -r -p "[S/n]: " response
-response=${response,,} 
+while true; do
+    mostrar_menu
 
-if [[ $response =~ ^(s|si|S|SI|)$ ]]; then
-    echo "Desplegando contenedor para el frontend..."
-   
-    # Desplegamos el proyecto de gettaskdone-server
-    docker-compose -f  $DOCKER_COMPOSE_FILE_FRONTED up --build
-else
-    echo "Desplegando proyecto desde cero..."
-    docker-compose -f $DOCKER_COMPOSE_DEVELOP up --build;
-    
-fi
+    read -r -p "Introduce tu opción [0-4]: " option
 
+    case $option in
+        1)
+            echo "Has seleccionado Modo Actualización (frontend)."
+            if confirmacion; then
+                echo "Actualizando y desplegando el frontend..."
+                docker-compose -f $DOCKER_COMPOSE_FILE_FRONTED stop
+                docker-compose -f $DOCKER_COMPOSE_FILE_FRONTED pull
+                docker-compose -f $DOCKER_COMPOSE_FILE_FRONTED up -d
+
+            else
+                echo "Operación cancelada."
+            fi
+            break
+            ;;
+        2)
+            echo "Has seleccionado Lanzar en el servidor."
+            if confirmacion; then
+                echo "Desplegando desde cero el proyecto en el servidor..."
+                docker-compose -f $DOCKER_COMPOSE_FILE_FRONTED up --build
+            else
+                echo "Operación cancelada."
+            fi
+            break
+            ;;
+        3)
+            echo "Has seleccionado Modo Desarrollador Backend."
+            if confirmacion; then
+                echo "Desplegando proyecto para desarrollo backend..."
+                docker-compose -f $DOCKER_COMPOSE_DEVELOP up --build
+            else
+                echo "Operación cancelada."
+            fi
+            break
+            ;;
+        4)
+            echo "Has seleccionado Eliminar todo (imagenes, contenedores) de docker."
+            if confirmacion; then
+                echo "Eliminando todo..."
+                docker rm $(docker stop $(docker ps -aq))
+                docker system prune -a --volumes
+            else
+                echo "Operación cancelada."
+            fi
+            break
+            ;;
+        0)
+            echo "Saliendo del script"
+            exit 0
+            ;;
+        *)
+            echo "Opción no válida. Intenta de nuevo."
+            ;;
+    esac
+done
