077974eec503774c9a2b3d4b8066d8418e2929fe
root <root@lopezgeraghty.com>
2024-03-08 11:11:43 +0200

    add cors

---
+++ src/main/java/com/gettasksdone/configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.gettasksdone.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+		.allowedOriginPatterns("*") // Permitir peticiones desde cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Cabeceras permitidas
+                .allowCredentials(true) // Permitir credenciales (por ejemplo, cookies)
+                .maxAge(3600); // Tiempo de validez de la preflight request en segundos
+    }
+}
