be57873cdcfa14299ad2c81ea6f64baa9deb88be
jcandela@ucm.es <jcandela@ucm.es>
2024-03-24 09:22:15 +0100

    sesion Se añade el certificado  autofirmado para la rama de desarrollo opción 4 24-03-2024

--- run.sh
+++ run.sh
@@ -5,6 +5,17 @@
 # debe permitirse actualizar un los contendores
 # debe tener la version de desarollo para el backend
 
+
+#Generar certificado
+# Configuración de contraseñas y alias
+KEYSTORE_PASSWORD=""
+ALIAS="tomcat"
+
+# Directorios y archivos
+KEYSTORE_DIRECTORY="src/main/resources/keystore"
+KEYSTORE_FILE="${KEYSTORE_DIRECTORY}/keystore.p12"
+DAYS_VALID=365 # Validez del certificado en días
+
 #anotaciones:
 # es mejor crear funciones para no tener redundancias.
 # añadida las anotaciones de David
@@ -25,8 +36,9 @@ function mostrar_menu() {
     echo "Selecciona una opción:"
     echo "1) Actualizar contenedor del frontend"
     echo "2) construir contenedores en el servidor desde cero"
-    echo "3) Modo Desarrollador Backend"
-    echo "4) Borrar todo (PELIGRO)"
+    echo "3) Modo Desarrollador Backend(David)"
+    echo "4) Modo Desarrollador Backend(Camilo/Jhimmy)"
+    echo "5) Borrar todo (PELIGRO)"
     echo "0) Para salir "
 }
 
@@ -66,11 +78,13 @@ while true; do
             fi
             break
             ;;
+
         3)
-            echo "Has seleccionado Modo Desarrollador Backend."
+            echo "Has seleccionado Modo Desarrollador Backend(David)."
             if confirmacion; then
                 echo "Desplegando proyecto para desarrollo backend..."
                 docker-compose -f $DOCKER_COMPOSE_FILE_FRONTED down -v
+                docker-compose -f $DOCKER_COMPOSE_DEVELOP down -v
                 docker-compose -f $DOCKER_COMPOSE_DEVELOP up --build
             else
                 echo "Operación cancelada."
@@ -78,6 +92,46 @@ while true; do
             break
             ;;
         4)
+            echo "Has seleccionado Modo Desarrollador Backend(Camilo/Jhimmy)."
+            if confirmacion; then
+                echo "Desplegando proyecto para desarrollo backend..."
+                docker-compose -f $DOCKER_COMPOSE_FILE_FRONTED down -v
+                docker-compose -f $DOCKER_COMPOSE_DEVELOP down -v
+                #Generar certificado:
+
+                # Fallas encontradas por si estan creadas de antes las clavesss
+                if [ -d "${KEYSTORE_DIRECTORY}" ]; then
+                    rm -rf "${KEYSTORE_DIRECTORY}"
+                fi
+
+                # Crear el directorio keystore si no existe
+                mkdir -p ${KEYSTORE_DIRECTORY}
+
+                # Paso 1: Generar la clave privada
+                openssl genpkey -algorithm RSA -out "${KEYSTORE_DIRECTORY}/key.pem" -pkeyopt rsa_keygen_bits:2048
+
+                # Paso 2: Crear el certificado autofirmado
+                openssl req -new -x509 -key "${KEYSTORE_DIRECTORY}/key.pem" -out "${KEYSTORE_DIRECTORY}/cert.pem" -days ${DAYS_VALID} \
+                    -subj "/C=ES/ST=Madrid/L=Madrid/O=TuOrganizacion/OU=TuDepartamento/CN=example.com"
+
+                # Paso 3: Convertir el certificado y la clave a formato PKCS12
+                openssl pkcs12 -export -in "${KEYSTORE_DIRECTORY}/cert.pem" -inkey "${KEYSTORE_DIRECTORY}/key.pem" \
+                            -out "${KEYSTORE_FILE}" -password pass:${KEYSTORE_PASSWORD} -name ${ALIAS}
+
+                # Paso 4: Cambiar los permisos del keystore para asegurar el archivo
+                chmod 600 "${KEYSTORE_FILE}"
+
+                # Listo
+                echo "Keystore autofirmado generado en: ${KEYSTORE_FILE}"
+
+
+                docker-compose -f $DOCKER_COMPOSE_DEVELOP up --build
+            else
+                echo "Operación cancelada."
+            fi
+            break
+            ;;
+        5)
             echo "Has seleccionado Eliminar todo (imagenes, contenedores) de docker."
             if confirmacion; then
                 echo "Eliminando todo..."
@@ -97,3 +151,6 @@ while true; do
             ;;
     esac
 done
+
+
+# docker-compose -f docker-compose.dev.yml up --build
